379df53e99b27487e22b954951a39928e316998c
zunehraBanu <banusyedazunehra@gmail.com>
2023-11-07 20:18:14 -0600

    Program number 2, Write a program in C++ to print the sum of two numbers.

--- p2Sum_of_2_num.cpp
+++ p2Sum_of_2_num.cpp
@@ -4,9 +4,12 @@ The sum of 29 and 30 is : 59*/
 using namespace std;
 int main()
 {
+    // Declare and initialize two numbers
     int num1 = 29;
     int num2 = 30;
+    // Calculate the sum of num1 and num2
     int sum = num1 + num2;
+    // Display the result
     cout << "The sum of " << num1 << " and " << num2 << " is : " << sum;
     return 0;
 }

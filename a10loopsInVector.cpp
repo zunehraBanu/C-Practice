27e386cb979c65b25c264a408824d3469d95f66b
zunehraBanu <banusyedazunehra@gmail.com>
2023-11-09 17:22:16 -0600

    Loops in vector

---
+++ a10loopsInVector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> integer;
+    //for loop
+  
+    for(int i=0; i<5; i++){
+        int element;
+        cin>>element;
+        integer.push_back(element);
+    }
+    for(int i=0; i<integer.size(); i++){
+        cout<<integer[i]<<" ";
+    }
+    //for each loop
+    for(int element: integer){
+        cout<<element<<" ";
+    }
+    cout<<endl;
+    //while loop
+   int idx=0;
+   while (idx<integer.size())
+   {
+    cout<<integer[idx]<<" ";
+    idx++;
+   }
+   
+    return 0;
+}

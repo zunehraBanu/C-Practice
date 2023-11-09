81199bb1679d9981bc50fc2d5184a0c5acfb2b78
zunehraBanu <banusyedazunehra@gmail.com>
2023-11-09 17:21:44 -0600

    Sizes in Vector.

---
+++ a9sizeVector.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+   vector<int> integer;
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+   integer.push_back(1);
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+
+   integer.push_back(2);
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+
+   integer.push_back(3);
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+   integer.resize(5);
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+
+   integer.resize(10);
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+
+   integer.pop_back();
+   integer.pop_back();
+   cout<<"Size: "<<integer.size()<<endl;
+   cout<<"Capacity: "<<integer.capacity()<<endl;
+    return 0;
+}

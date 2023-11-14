ae62520af510ffa6fd4faace836c72b0d8766f09
zunehraBanu <banusyedazunehra@gmail.com>
2023-11-13 22:45:54 -0600

    Find the total number of tripltes in the array whose sum is equal to the given value x.

---
+++ a24Triplets.cpp
@@ -0,0 +1,35 @@
+// Find the total number of tripltes in the array whose sum is equal to the given value x.
+#include <iostream>
+using namespace std;
+#include <vector>
+int main()
+{
+    cout << "Enter 10 numbers: " << endl;
+    vector<int> numbers;
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        int elemement;
+        cin >> elemement;
+        numbers.push_back(elemement);
+    }
+    int x;
+    cout << "Enter X: " << endl;
+    cin >> x;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            for (int h = j + 1; h < 10; h++)
+            {
+                if (numbers[j] + numbers[i] + numbers[h] == x)
+                {
+                    count = count + 1;
+                }
+            }
+        }
+    }
+
+    cout << "The total number of pairs in the array whose sum is equal to the given value x: " << count;
+    return 0;
+}

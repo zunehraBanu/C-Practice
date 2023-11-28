247dee7b8f411609bdceb6277f243d128aa1d702
zunehraBanu <banusyedazunehra@gmail.com>
2023-11-27 23:07:21 -0600

    Added Comments.

--- l4PrintZeros.cpp
+++ l4PrintZeros.cpp
@@ -41,16 +41,22 @@ Do you want to do it again (y/n): n*/
 using namespace std;
 int main()
 {
+    // Variable to store user input for repeating the program
     char input;
 
+    // Main do-while loop to repeat the program based on user input
     do
     {
+        // Variable to store the user-entered number
         int number;
+        // Prompt the user to enter a number between 1 and 50
         cout << "Enter a number between 1 and 50: ";
 
+        // Input validation loop to ensure the entered number is within the valid range
         do
         {
             cin >> number;
+            // Check if the entered number is outside the valid range
             if (number < 1 || number > 50)
             {
                 cout << "Invalid number - try again!!!" << endl;
@@ -58,17 +64,18 @@ int main()
             }
         } while (number < 1 || number > 50);
 
+        // Display the number of zeros based on the user-entered number using a for loop
         cout << number << " zeros: ";
         for (int i = 0; i < number; i++)
         {
             cout << "0";
         }
         cout << endl;
-
+        // Prompt the user if they want to repeat the program
         cout << "\nDo you want to do it again (y/n): ";
         cin >> input;
 
-    } while (input == 'y' || input == 'Y');
+    } while (input == 'y' || input == 'Y'); // Continue the loop if the user enters 'y' or 'Y'
 
     cout << endl;
     return 0;

585ac0e46aac11c633e36fdff1f4dfc76914db6f
zunehraBanu <banusyedazunehra@gmail.com>
2023-11-28 23:08:00 -0600

    Added comments.

--- l3NestedLoops.cpp
+++ l3NestedLoops.cpp
@@ -27,19 +27,26 @@ Enter triangle height: 3
 using namespace std;
 int main()
 {
+    // Declare variables to store user input
     char character;
     int height;
+    // Prompt the user to enter a character
     cout << "Enter a character: ";
     cin >> character;
+    // Prompt the user to enter the triangle height
     cout << "Enter triangle height: ";
     cin >> height;
     cout << endl;
+    // Outer loop for each row of the triangle
     for (int i = 1; i <= height; i++)
     {
+        // Inner loop for printing characters in each row
         for (int j = 1; j <= i; j++)
         {
+            // Print the specified character followed by a space
             cout << character << " ";
         }
+        // Move to the next line after completing a row
         cout << endl;
     }
 
